c0691d7ba3e8d7116c3c67ae22740fe2d2ef5e2c
miSkoMV <36664000+miskomv@users.noreply.github.com>
2020-01-21 18:13:27 +0100

    Isolate class

--- Loader.vue
+++ Loader.vue
@@ -1,16 +1,16 @@
 <template>
     <transition enter-active-class="fadeIn" leave-active-class="fadeOut" :duration="500">
-        <div class="loader" v-if="visible">
+        <div class="vue-ac-loader" v-if="visible">
             <img src="loading.svg" alt="Loading">
         </div>
     </transition>
 </template>
 <style scoped>
-    .loader { position: fixed; top: 0; left: 0; width: 100%; height: 100%; z-index: 99999; background: rgba(0, 0, 0, 0.75); transition: all 0.5s linear; opacity: 1}
-    .loader { display: flex; justify-content: center; align-items: center; align-content: center}
-    .loader img { width: 200px; height: 200px;}
-    .loader.fadeIn { opacity: 1}
-    .loader.fadeOut { opacity: 0}
+    .vue-ac-loader { position: fixed; top: 0; left: 0; width: 100%; height: 100%; z-index: 99999; background: rgba(0, 0, 0, 0.75); transition: all 0.5s linear; opacity: 1}
+    .vue-ac-loader { display: flex; justify-content: center; align-items: center; align-content: center}
+    .vue-ac-loader img { width: 200px; height: 200px;}
+    .vue-ac-loader.fadeIn { opacity: 1}
+    .vue-ac-loader.fadeOut { opacity: 0}
 </style>
 <script>
     export default {
