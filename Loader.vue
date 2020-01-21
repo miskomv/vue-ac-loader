35545987f8bcb1a8bcab3d1565cbbc2b5b3e8d03
miSkoMV <36664000+miskomv@users.noreply.github.com>
2020-01-21 18:46:20 +0100

    Fade animation

--- Loader.vue
+++ Loader.vue
@@ -1,16 +1,16 @@
 <template>
-    <transition enter-active-class="fadeIn" leave-active-class="fadeOut" :duration="500">
+    <transition :duration="500" name="vue-ac-fade">
         <div class="vue-ac-loader" v-if="visible">
             <img src="./loading.svg" alt="Loading">
         </div>
     </transition>
 </template>
 <style scoped>
-    .vue-ac-loader { position: fixed; top: 0; left: 0; width: 100%; height: 100%; z-index: 99999; background: rgba(0, 0, 0, 0.75); transition: all 0.5s linear; opacity: 1}
+    .vue-ac-loader { position: fixed; top: 0; left: 0; width: 100%; height: 100%; z-index: 99999; background: rgba(0, 0, 0, 0.75); transition: all 0.5s linear;}
     .vue-ac-loader { display: flex; justify-content: center; align-items: center; align-content: center}
     .vue-ac-loader img { width: 200px; height: 200px;}
-    .vue-ac-loader.fadeIn { opacity: 1}
-    .vue-ac-loader.fadeOut { opacity: 0}
+    .vue-ac-loader.vue-ac-fade-enter { opacity: 0 }
+    .vue-ac-loader.vue-ac-fade-leave-active { opacity: 0 }
 </style>
 <script>
     export default {
